d8e71af2bf860db9f1ba08b8717ed2e64ca95919
xxxofficial <antonsoldatko@gmail.com>
2025-05-31 02:29:28 +0300

    Update mapObject6.js

--- mapObject6.js
+++ mapObject6.js
@@ -1,13 +1,11 @@
 function mapObject(obj, callback) {
-    const output = {};
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        output[key] = callback(obj[key]);
-      }
-    }
-    return output;
+  const output = {};
+  for (const key in obj) {
+    output[key] = callback(obj[key]);
   }
+  return output;
+}
   
-  const nums = { a: 4, b: 6, c: 23 };
-  const multTwo = mapObject(nums, x => x * 2);
-  console.log(multTwo); 
+const nums = { a: 4, b: 6, c: 23 };
+const multTwo = mapObject(nums, x => x * 2);
+console.log(multTwo); 
